1d993010f930a06cfdf058cd3ed61bed71ba02ec
Eduardo Ismael García Pérez <eduardo78d@gmail.com>
2018-12-05 19:23:55 -0600

    Cambios

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ class Main{
   public static void main(String[] args){
       System.out.println("Hola Mundo desde java, saludos a Jenkins!!");
 
-      System.out.println("Ahora nos encontramos en la rama Develpment!!");
-
-      System.out.printlln("Mensaje en color verde");
+      System.out.println("Se envían las notificaciones");
   }
 
 }
